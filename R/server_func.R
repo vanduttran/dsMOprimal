d470f218a343bfe6af380e7311e38856f6c89c6c
Thuong Tran <thuong.tran@sib.swiss>
2021-04-23 09:45:13 +0200

    WIP

--- R/server_func.R
+++ R/server_func.R
@@ -44,7 +44,7 @@ center <- function(x, na.rm = TRUE) {
         y[is.na(y)] <- 0
     }
     y <- y[order(rownames(y)), ]
-    y <- head(y, 10)
+    y <- head(y, 100)
     return (scale(y, center=TRUE, scale=FALSE))
 }
 
