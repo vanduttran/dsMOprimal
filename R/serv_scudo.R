f9f7efe38e0796670120a3a0816fc1452e2e9e33
Riccardo-Cpt <riccardo.9giuliani.6@gmail.com>
2021-07-21 15:18:16 +0200

    message

--- R/serv_scudo.R
+++ R/serv_scudo.R
@@ -46,28 +46,17 @@ computeWeights <- function(expressionData, indexMatrix, top = 2, bottom= 2, topW
 
 
 
-#' @title computeWeights
+#' @title aggRownames
 #'
 #' Weight most and least regulated features in original dataMatrix accordingly to ranking 
 #' @param expressionData A matrix or data frame, samples in rows and features in columns
-#' @param indexMatrix rank matrix of same dimension of expressionData matrix
-#' @param top which most regulated genes we consider
-#' @param bottom which least regulated genes we consider
-#' @param topWeight weight associated to top elements
-#' @param bottomWeight weight associated to bottom elements
-#' @return outWeiight matrix of the same dimension of expressionData
+#' @return rownames of expressonData
 #' @export
 sampleNames <- function(expressionData){
 
     return(rownames(expressionData))
 
- }
-
-
-
-
-
-
+}
 
 #' @title Federate SSCP on weighted data
 #' @description Function for computing the federated SSCP matrix
