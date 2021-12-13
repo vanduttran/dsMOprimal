421ebf81bc6c3690970df2c65fbbd46d68e77129
Thuong Tran <thuong.tran@sib.swiss>
2021-12-13 18:08:24 +0100

    WIP

--- R/server_func.R
+++ R/server_func.R
@@ -456,16 +456,32 @@ sumMatrices <- function(dsc = NULL) {
 #' @param some.object the object to be encoded
 #' @return encoded text with offending characters replaced by strings
 #' @keywords internal
-.encode.arg <- function(some.object){
-  encoded <- RCurl::base64Encode(jsonlite::toJSON(some.object, null = 'null'));
-  # go fishing for '+', '/' and '=', opal rejects them :
-  my.dictionary <- c('\\/' = '-slash-', '\\+' = '-plus-', '\\=' = '-equals-')
-  sapply(names(my.dictionary), function(x){
-    encoded[1] <<- gsub(x, my.dictionary[x], encoded[1])
-  })
-  return(paste0(encoded[1],'base64'))
-
+.encode.arg <- .encode.arg <- function(some.object, serialize.it = FALSE){
+    if(serialize.it){
+        encoded <- paste0(RCurl::base64Encode(jsonlite::serializeJSON(some.object)), 'serialized')
+    } else {
+        encoded <- RCurl::base64Encode(jsonlite::toJSON(some.object, null = 'null'))
+    }
+    # go fishing for '+', '/' and '=', opal rejects them :
+    my.dictionary <- c('\\/' = '-slash-', '\\+' = '-plus-', '\\=' = '-equals-')
+    sapply(names(my.dictionary), function(x){
+        encoded[1] <<- gsub(x, my.dictionary[x], encoded[1])
+    })
+    return(paste0(encoded[1],'base64'))
+    
 }
+
+# 
+#     function(some.object){
+#   encoded <- RCurl::base64Encode(jsonlite::toJSON(some.object, null = 'null'));
+#   # go fishing for '+', '/' and '=', opal rejects them :
+#   my.dictionary <- c('\\/' = '-slash-', '\\+' = '-plus-', '\\=' = '-equals-')
+#   sapply(names(my.dictionary), function(x){
+#     encoded[1] <<- gsub(x, my.dictionary[x], encoded[1])
+#   })
+#   return(paste0(encoded[1],'base64'))
+# 
+# }
 
 
 #' @title Federated covariance matrix
