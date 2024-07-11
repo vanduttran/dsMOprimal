667288141504ac3fa06c4160cfd9c5a6b44367ad
Thuong Tran <thuong.tran@sib.swiss>
2024-07-11 17:52:20 +0200

    WIP

--- R/misc.R
+++ R/misc.R
@@ -6,19 +6,22 @@
 #'  See \code{dsSwissKnifeClient:::.encode.arg}.
 #' @param some.object the object to be encoded
 #' @return encoded text with offending characters replaced by strings
+#' @importFrom RCurl base64Encode
+#' @importFrom jsonlite serializeJSON toJSON
 #' @keywords internal
-.encode.arg <- function(some.object, serialize.it = TRUE, digits=20){
-    if(serialize.it){
-        encoded <- paste0(RCurl::base64Encode(jsonlite::serializeJSON(some.object, digits=digits)), 'serialized')
+.encode.arg <- function(some.object, serialize.it = TRUE, digits = 20) {
+    if (serialize.it) {
+        encoded <- paste0(base64Encode(
+            serializeJSON(some.object, digits=digits)), 'serialized')
     } else {
-        encoded <- RCurl::base64Encode(jsonlite::toJSON(some.object, null = 'null'))
+        encoded <- base64Encode(toJSON(some.object, null = 'null'))
     }
     # go fishing for '+', '/' and '=', opal rejects them :
     my.dictionary <- c('\\/' = '-slash-', '\\+' = '-plus-', '\\=' = '-equals-')
     sapply(names(my.dictionary), function(x){
         encoded[1] <<- gsub(x, my.dictionary[x], encoded[1])
     })
-    return(paste0(encoded[1],'base64'))
+    return (paste0(encoded[1],'base64'))
 }
 
 
@@ -30,27 +33,31 @@
 #' @details It looks for the string 'base64' in the argument to determine if it's encoded
 #' @param some.thing the thing to be decoded and deserialized from json if necessary
 #' @return the decoded and deserialized argument
+#' @importFrom jsonlite unserializeJSON fromJSON
+#' @importFrom RCurl base64Decode
 #' @keywords internal
-.decode.arg <- function(some.thing, simplifyMatrix = FALSE){
-    if(length(some.thing) == 1 && grepl('base64', some.thing, ignore.case = TRUE)){
-        some.thing <- gsub('base64', '', some.thing, ignore.case =TRUE)
+.decode.arg <- function(some.thing, simplifyMatrix = FALSE) {
+    if (length(some.thing) == 1 &&
+        grepl('base64', some.thing, ignore.case = TRUE)){
+        some.thing <- gsub('base64', '', some.thing, ignore.case=TRUE)
         serialized <- FALSE
-        if(grepl('serialized', some.thing, ignore.case = TRUE)){
+        if (grepl('serialized', some.thing, ignore.case=TRUE)) {
             serialized <- TRUE
-            some.thing <- gsub('serialized', '', some.thing, ignore.case =TRUE)
+            some.thing <- gsub('serialized', '', some.thing, ignore.case=TRUE)
         }
         my.dictionary = c('-plus-' = '+', '-slash-' = '/', '-equals-' = '=')
         sapply(names(my.dictionary), function(x){
             some.thing <<- gsub(x, my.dictionary[x], some.thing)
         })
         #
-        if(serialized){
-            some.thing <- jsonlite::unserializeJSON(RCurl::base64Decode(some.thing))
+        if (serialized) {
+            some.thing <- unserializeJSON(base64Decode(some.thing))
         } else {
-            some.thing <- jsonlite::fromJSON(RCurl::base64Decode(some.thing), simplifyMatrix = simplifyMatrix)
+            some.thing <- fromJSON(base64Decode(some.thing),
+                                   simplifyMatrix = simplifyMatrix)
         }
     }
-    return(some.thing)
+    return (some.thing)
 }
 
 
@@ -69,7 +76,8 @@
         SUCCESS <- FALSE
         while (j < nTry && !SUCCESS) {
             tryCatch({
-                opals[i] <- datashield.login(logins[logins$server == i, , drop = FALSE])
+                opals[i] <- datashield.login(logins[logins$server == i, ,
+                                                    drop = FALSE])
                 SUCCESS <- TRUE
             }, error = function(e){
                 Sys.sleep(1)
@@ -97,8 +105,10 @@
     objs <- ls(envir, all.names = TRUE)
     ret <- list()
     ret[[start]] <- objs
-    more.envs <- names(which(sapply(objs, function(x) is.environment(get(x)))==TRUE))
-    c(ret,sapply(more.envs,function(x) ls(get(x), all.names = TRUE), USE.NAMES = TRUE, simplify = FALSE))
+    more.envs <- names(which(sapply(objs, function(x)
+        is.environment(get(x)))==TRUE))
+    c(ret,sapply(more.envs,function(x) ls(get(x), all.names = TRUE),
+                 USE.NAMES = TRUE, simplify = FALSE))
     
 }
 
