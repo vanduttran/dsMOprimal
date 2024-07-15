7e4a8f2c1871cb732ea70b8c2998552a463c350f
Thuong Tran <thuong.tran@sib.swiss>
2024-07-15 12:03:42 +0200

    WIP

--- R/server_func.R
+++ R/server_func.R
@@ -423,9 +423,9 @@ matrix2DscFD <- function(value) {
     # TOIMPROVE: use .decode.arg(valued) instead with input from .encode.arg(serialize.it=T)
     #tcp <- .decode.arg(valued)
     #tcp <- do.call(rbind, .decode.arg(valued))
-    
-    tcp <- as.matrix(read_ipc_stream(.decode.arg(value)))
     save(value, "/tmp/value.RData")
+    tcp <- as.matrix(read_ipc_stream(.decode.arg(value)))
+    save(value, "/tmp/tcp.RData")
     dscbigmatrix <- describe(as.big.matrix(tcp, backingfile = ""))
     save(dscbigmatrix, "/tmp/dsc.RData")
     rm(list=c("tcp"))
