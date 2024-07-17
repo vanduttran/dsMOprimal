012a25986adcaa24f230d4730107920ab04a91d6
Thuong Tran <thuong.tran@sib.swiss>
2024-07-17 11:18:05 +0200

    WIP

--- R/server_func.R
+++ R/server_func.R
@@ -636,7 +636,7 @@ tripleProdChunk <- function(x, mate, chunk = 500, mc.cores = 1) {
             #y <- get(paste("crossProdSelf", pid, sep='_'), pos=1)#, envir = .GlobalEnv) #parent.frame())
             y <- get(paste("pushed", mate, sep='_'), pos=1)
             print(y[[i]][1:3,1:3])
-            stopifnot(isSymmetric(y[[i]]))
+            stopifnot(isSymmetric(y[[i]], check.attributes=F))
             ## NB. this computation of tcpblocks could be done more efficiently
             ## with y being a chunked matrix in bigmemory
             tp <- tcrossprod(x[[i]], tcrossprod(x[[i]], y[[i]]))
